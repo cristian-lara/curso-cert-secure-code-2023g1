62a41987094f9fbd122f8a9ec360c77361003b0c
Cristian Lara <cristian89lara@gmail.com>
2023-04-18 19:40:48 -0500

    feat:  proyecto creado

---
+++ api-curso/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController, UserController, AuthController],
+  providers: [AppService, UserService, AuthService],
+})
+export class AppModule {}

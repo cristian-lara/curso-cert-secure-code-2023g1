4bbb68c046192209229409c1046c9fe080f400a9
Cristian Lara <cristian89lara@gmail.com>
2023-04-18 21:58:29 -0500

    feat: modulo jwt configurado

--- api-curso/src/app.module.ts
+++ api-curso/src/app.module.ts
@@ -5,9 +5,18 @@ import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: 'no poner nunca aqui el secreto quemado',
+      signOptions: {
+        expiresIn: '3600s'
+      }
+    })
+  ],
   controllers: [AppController, UserController, AuthController],
   providers: [AppService, UserService, AuthService],
 })

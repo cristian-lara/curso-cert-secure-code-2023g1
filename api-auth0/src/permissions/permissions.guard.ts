816d13169d8596e2dae5732b94b58349f98044d5
Cristian Lara <cristian89lara@gmail.com>
2023-04-24 20:22:55 -0500

    feat: permisos rescatados del controlador

--- api-auth0/src/permissions/permissions.guard.ts
+++ api-auth0/src/permissions/permissions.guard.ts
@@ -1,16 +1,28 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class PermissionsGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector){}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean>  {
     const [req] = context.getArgs();
     const payloadAuth = req.auth;
-    const permisosUsuario = payloadAuth.permissions || [];
+    const permisosUsuariodeAutho0 = ['leer:autor', 'permisos inventado'];
     // ['leer:autor','crear:autor', 'leer:libro']
-    console.log('request', permisosUsuario);
+    console.log('request', permisosUsuariodeAutho0);
+    const permisosDeControlador = this.reflector.get('permissions', context.getHandler()) || [];
+    console.log('permisos de controlador', permisosDeControlador);
+    const tienePermisosElUsuario = permisosDeControlador.every(
+      permisosControlador => permisosUsuariodeAutho0.includes(permisosControlador)
+    )
+    console.log('tienen permisos el usuario', tienePermisosElUsuario);
+
+    if(permisosUsuariodeAutho0.length === 0){
+      throw new UnauthorizedException();
+    }
     return true;
   }
 }
